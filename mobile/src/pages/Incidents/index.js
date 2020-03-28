70149c53ec59e003f73913d3ab2679f3f8ddca18
Rafaela Marconato <raffamarconato@gmail.com>
2020-03-28 12:45:58 -0300

    estrutura parcial do mobile

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+
+import logoImg from "../../assets/logo.png";
+import styles from "./styles";
+
+export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigationToDetail() {
+    navigation.navigate("Details");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTexBold}>0 casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e seja um Herói.
+      </Text>
+
+      <FlatList
+        style={styles.incidentList}
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={incident => String(incident)}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+              style={styles.detailButton}
+              onPress={navigationToDetail}
+            >
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
